a1a9f177db67c9586ab115b698764197a019c01e
anjagutmann <anja.gutmann@hfg-gmuend.de>
2023-03-26 18:05:57 +0200

    03_1_Commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,47 @@
-$ (function () {
+// let stage;
+// let stageHeight;
+// let stageWidth;
 
+$(function () {
+    // stage = $('#renderer')
+    // stageHeight = stage.innerHeight();
+    // stageWidth = stage.innerWidth();
+
+    // for (let i=0; i < data.length; i++){
+    //     let currentTerm = data[i];
+    //     let title = currentTerm.title;
+    //     let shortExplanation = currentTerm.short_explanation;
+    //     let tags = currentTerm.tags;
+    // }
+
+    $.each(data, function(index, value) {
+        let title = value.title;
+        let shortExplain = value.short_explanation;
+        let explain = value.explanation;
+        let tags = value.tags;
+
+        //Titel
+        $('.tile-headline h3').text(value.title);
+        $('.tile-text p').text(value.short_explanation);
+        $('.hover-text').text(value.explanation);
+        // $('.tile-tags li').text(value.tags);
+
+        //Tags
+        let tagList = $('.tile-tags ul');
+        tagList.empty(); // clear any existing tags
+
+        $.each(tags, function(index, tag) {
+            let tagItem = $('<li>').text(tag);
+            tagList.append(tagItem);
+        });
+
+        //Text einfügen
+        title.text(value.title);
+        shortExplain.text(value.short_explanation);
+        explain.text(value.explanation);
+        // tags.text(value.tags);
+
+        $('#tiles').append(tile);
+    });
 });
+
